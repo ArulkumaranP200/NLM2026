1d8495703aa1b429ddc540670240e9a1c02ea730
ArulkumaranP200 <aswanthadhi861@gmail.com>
2026-07-15 22:50:14 +0530

    Add test endpoint to urls.py

--- backend/matrimony/urls.py
+++ backend/matrimony/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+def test(request):
+    return HttpResponse("Backend is updated!")
 urlpatterns = [
+    path("test/", test),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/profiles/', include('profiles.urls')),
