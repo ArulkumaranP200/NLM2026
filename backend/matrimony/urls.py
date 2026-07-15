a29e6b6283d522888845c50e7db4841fb05c81b7
ArulkumaranP200 <aswanthadhi861@gmail.com>
2026-07-15 23:24:33 +0530

    Update urls.py

--- backend/matrimony/urls.py
+++ backend/matrimony/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
-def test(request):
-    return HttpResponse("Backend is updated!")
-
 urlpatterns = [
-    path("test/", test),
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/profiles/", include("profiles.urls")),
